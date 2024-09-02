4778acd5e6bf0a63afdd19366c5abb273c9648c3
v <t.shagin@gmail.com>
2024-09-02 23:27:59 +0300

    feat: Добавлен класс GetOrCreate

--- database/create.py
+++ database/create.py
@@ -1,3 +1,5 @@
+from pyrogram.types import Message, User, Chat
+
 from database.models import active_models
 from database.db_init import db
 
@@ -10,3 +12,18 @@ def create_tables() -> None:
     with db:
         db.create_tables(active_models)
     print("Модели базы данных успешно созданы!")
+
+
+class GetOrCreate:
+    def __init__(
+            self,
+            *,
+            request: Message | None = None,
+            chat: Chat | None = None,
+            user: User | None = None
+    ) -> None:
+        if not (request or (chat and user)):
+            raise Exception("Не хватает аргументов! (Вызвано классом database/create/GetOrCreate)")
+        self.user: User = request.from_user if user is None else user
+        self.chat: Chat = request.chat if chat is None else chat
+
